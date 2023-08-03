63967ce9d2405b2ef8fd60bb4608d4f4e31284ce
GONZALO MORENO <gonzalo_moreno@keysight.com>
2023-08-04 01:18:23 +0200

    Add unit tests project

---
+++ TestBenchUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using TestBench;
+namespace TestBenchUnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
